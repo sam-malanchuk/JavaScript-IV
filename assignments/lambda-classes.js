b41ebf4ea25c03aa1c4e49e5dd5f49afa7e4c608
sam-malanchuk <malanchuksam@gmail.com>
2019-04-25 19:19:44 -0400

    created Person constructor in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(obj) {
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+        this.gender  = obj.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
